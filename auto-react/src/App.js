1a7fa8cee3363fca2f9482eaf448c84b50ccabf3
nada stitou <nadastito1087@gmail.com>
2022-10-02 00:22:16 +0100

    table

--- auto-react/src/App.js
+++ auto-react/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Footer from "./components/footer";
 import Table from "./components/table";
-
+import { num  as number} from "./components/students";
+import { num } from "./components/student2";
 
 class App extends React.Component{
 render(){
+  console.log(number)
   return(
 
     <div>
-    <Footer />
-    <Table />
+      
+      <h1>{number[1].stagiaire4}</h1>
+      <h1> {num[1]}</h1>
     </div>
   )
 }
