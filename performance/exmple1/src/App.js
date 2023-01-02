7fc9248539174f18c9a60887b5ac03903ed45f44
stitou-nada <nadastito1087@gmail.com>
2023-01-02 11:36:55 +0100

    add

--- performance/exmple1/src/App.js
+++ performance/exmple1/src/App.js
@@ -6,12 +6,16 @@ import './App.css';
 function App() {
   const [CountA, setCountA] = React.useState(0);
   const [CountB, setCountB] = React.useState(0);
+ 
 
+  
   return (
     <div>
       <Count name="A" value={CountA} />
       <Count name="B" value={CountB} />
-      <button onClick={() => {console.log("Click button");setCountA(CountA + 1);}}>
+      <button onClick={() =>
+         {console.log("Click button");
+         setCountA(CountA + 1);}}>
         Increment counter A
       </button>
     </div>
