4a903bb83ec7c37e3479b0e00603de9b41619bad
stitou-nada <nadastito1087@gmail.com>
2023-01-02 09:57:00 +0100

    add

--- performance/src/App.js
+++ performance/src/App.js
@@ -1,32 +1,3 @@
-// import { useState, useCallback } from "react";
-// import ReactDOM from "react-dom/client";
-// import Todos from "./component/todos";
-
-// const App = () => {
-
-//   const [count, setCount] = useState(0);
-//   const [todos, setTodos] = useState([]);
-
-//   const increment = () => {
-//     setCount((c) => c + 1);
-//   };
-//   const addTodo = useCallback(() => {
-//     setTodos((t) => [...t, "New Todo"]);
-//   }, [todos]);
-//   console.log("count render");
-
-//   return (
-//     <>
-//       <Todos todos={todos} addTodo={addTodo} />
-//       <hr />
-//       <div>
-//         Count: {count}
-//         <button onClick={increment}>+</button>
-//       </div>
-//     </>
-//   );
-// };
-// export default App;
 
 import logo from './logo.svg';
 import React from 'react';
