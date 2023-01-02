0bd9c55765a11de5e8175c526bbb8f047fd56033
stitou-nada <nadastito1087@gmail.com>
2023-01-02 13:17:54 +0100

    add

--- performance/useMemo/src/App.js
+++ performance/useMemo/src/App.js
@@ -1,37 +1,37 @@
-import { useState, useMemo } from "react";
+import { useState,  memo } from "react";
 import ReactDOM from "react-dom/client";
 import React from "react";
 
 
-const App = () => {
+ const App = () => {
   const [count, setCount] = useState(0);
 
-  const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
   const increment = () => {
     setCount((c) => c + 1);
+    console.log("ra ki t3awed")
   };
   
-  const expensiveCalculation = (num) => {
-    console.log("Calculating...");
-    for (let i = 0; i < 1000000000; i++) {
-      num += 1;
-    }
-    return num;
-  };
+  // const expensiveCalculation = (num) => {
+  //   console.log("Calculating...");
+  //   for (let i = 0; i < 1000000000; i++) {
+  //     num += 1;
+  //   }
+  //   return num;
+  // };
   
   return (
       <div>
         Count: {count}
         <button onClick={increment}>+</button>
         <h2>Expensive Calculation</h2>
-        {calculation}
+      
+        hhhh
       </div>
 
   );
-};
+ };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 
-export default App;
+
+export default memo(App);
