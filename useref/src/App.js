1966743475ca6c8e629c1a6f7defa5f8f02322e6
stitou-nada <nadastito1087@gmail.com>
2023-01-03 10:57:00 +0100

    useRef

--- useref/src/App.js
+++ useref/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const firstname = React.useRef()
+  const age =React.useRef()
+  const image =React.useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <input type="text" ref={firstname}></input>
+    <input type="number" ref={age}></input>
+    <input type="file"  multiple ref={image}></input>
+    <button onClick={()=>console.log(firstname.current.value)}>Print firstname</button>
+    <button onClick={()=>console.log(age)}>Print age</button>
+    <button onClick={()=>console.log(image.current.files)}>input image</button>
     </div>
   );
 }
