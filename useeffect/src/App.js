d2a889ed331f8fa5cfc83153b330421d86a879f2
stitou-nada <nadastito1087@gmail.com>
2023-01-03 10:38:31 +0100

    use effect

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const[title , setTitle]= React.useState("react hooks");
+
+
+  React.useEffect(()=>{
+    // component did mount
+     document.title=title;
+    return ()=>{
+    // component will unmount
+    };
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={()=>setTitle("hello")}>change title</button>
     </div>
   );
 }
